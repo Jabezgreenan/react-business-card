acecb7f80a340bf456a363e96b2338dec7db635c
Jabez Greenan <128314599+Jabezgreenan@users.noreply.github.com>
2026-09-13 23:24:12 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ export default function App(){
             
             <section className="about">
                 <h2>About</h2>
-                <p>I am a frontend developer with a particular interest in
+                <p>I am a fullstack developer with a particular interest in
                 making things simple and automating daily tasks. I try to keep up with 
                 security and best practices,
                 and am always looking for new things to learn.
@@ -32,8 +32,7 @@ export default function App(){
 
             <section className="interests">
                 <h2>Interests</h2>
-                <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. 
-                Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+                <p>Music, instruments, technology, photography/videography, learning new skills. 
                 </p>
             </section>
 
