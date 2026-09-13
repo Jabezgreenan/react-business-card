36936074d61c736106470adaf862dcc7804f35bb
Jabez Greenan <128314599+Jabezgreenan@users.noreply.github.com>
2026-09-13 23:24:54 +0200

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,6 +2,6 @@
 
 export default function Button({text}) {
     return (
-        <a className={`btn ${text.toLowerCase()}`} href={text === "linkedin"?"https://www.linkedin.com/in/jabez-greenan-60b434271/":"mailto:jabezgreenan@gmail.com"}><img src={`src/assets/images/${text}.png`}/>{text}</a> 
+        <a className={`btn ${text.toLowerCase()}`} href={text === "LinkedIn"?"https://www.linkedin.com/in/jabez-greenan-60b434271/":"mailto:jabezgreenan@gmail.com"}><img src={`src/assets/images/${text}.png`}/>{text}</a> 
     )
 }
